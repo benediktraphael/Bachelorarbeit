b9fcce53008e51f007b6b3af37417ea76b9405d5
Benedikt Berger <benedikt.berger@alumni.fh-aachen.de>
2025-01-14 19:16:42 +0100

    IDK

--- SheetGen/Lines.py
+++ SheetGen/Lines.py
@@ -7,14 +7,14 @@ import time
 #der DINA4-Zettel ist auf dem Bildschirm mit der Auflösung ? im Vollbild bei 33,33%
 
 
-clefs = []
-keys = []
-lineBrush = (-150, 200)
-lineStart = (-1645,350)
-lineLength = 715
-systemHeight = 35
+#clefs = []
+#keys = []
+#lineBrush = (-150, 200)
+#lineStart = (-1645,350)
+#lineLength = 715
+#systemHeight = 35
 
-def drawLines():
+def drawLines(lineBrush, lineStart, lineLength, systemHeight):
     
     pyautogui.moveTo(lineBrush)
     pyautogui.click()
@@ -35,7 +35,7 @@ def drawLines():
         j = j+1
 
 
-def drawClefs(clef):
+def drawClefs(clef, clefs, lineStart, systemHeight):
     match clef:
         case 71:
             offset = 4
@@ -44,7 +44,6 @@ def drawClefs(clef):
         case 50:
             offset = 0
             pyautogui.moveTo(clefs[0])
-    #select Notenschlüssel
     pyautogui.click()
     pyautogui.moveTo(lineStart[0]+4, lineStart[1]+2/5*systemHeight+offset)
     
@@ -56,7 +55,7 @@ def drawClefs(clef):
     return
 
 
-def drawKey(key):
+def drawKey(key, keys, lineStart, systemHeight):
     accidentals_order = [0,3,1,4,2]
     used_clef = [1,3,5,8,10]
     #b: auf Linie um zwischen Linie.
@@ -76,12 +75,14 @@ def drawKey(key):
         for i in range(0, key, (-1 if key < 0 else 1)):
             pyautogui.moveRel(0, used_acc[i])
             pyautogui.click()
-            pyautogui.moveRel(7, -used_acc[i])
+            pyautogui.moveRel(1/5*systemHeight, -used_acc[i])
     return
 
 
-def prepareSheet(clef, key):
-    drawLines()
-    drawClefs(clef)
-    drawKey(key)
+def prepareSheet(clef, key, lineBrush, lineLength, systemHeight, lineStart, clefs, keys):
+    drawLines(lineBrush, lineStart, lineLength, systemHeight)
+    drawClefs(clef, clefs, lineStart, systemHeight)
+    drawKey(key, keys, lineStart, systemHeight)
     return
+
+
--- SheetGen/main.py
+++ SheetGen/main.py
@@ -11,22 +11,23 @@ def setCoordinates():
     writeNotes.brushes = [coordinates[6], coordinates[7], coordinates[2],coordinates[3], coordinates[8], coordinates[9], coordinates[4], coordinates[5]]
     writeNotes.additional_brushes = [coordinates[10], coordinates[11], coordinates[13], coordinates[12], coordinates[14]]
 
-    Lines.clefs = [coordinates[15], coordinates[16]]
-    Lines.keys = [coordinates[13], coordinates[14]]
+    Lines_clefs = [coordinates[15], coordinates[16]]
+    Lines_keys = [coordinates[13], coordinates[14]]
 
-    Lines.lineBrush = coordinates[11]
+    Lines_lineBrush = coordinates[11]
 
     paper_size = (coordinates[1][0]-coordinates[0][0], coordinates[1][1]-coordinates[0][1])
 
     effektive_paper_heigth = paper_size[1] * 0.85
 
-    Lines.systemHeight = int(effektive_paper_heigth/25)
+    Lines_systemHeight = int(effektive_paper_heigth/25)
 
 
-    Lines.lineLength = paper_size[0] * 0.85
-    Lines.lineStart = (coordinates[0][0] + (paper_size[0] * 0.075), coordinates[0][1] + 0.12 * paper_size[1])
+    Lines_lineLength = paper_size[0] * 0.85
+    Lines_lineStart = (coordinates[0][0] + (paper_size[0] * 0.075), coordinates[0][1] + 0.12 * paper_size[1])
 
+
+    Lines.prepareSheet(71, 0, Lines_lineBrush, Lines_lineLength, Lines_systemHeight, Lines_lineStart, Lines_clefs, Lines_keys)
     return
 
 setCoordinates()
-Lines.prepareSheet(71, 3)
